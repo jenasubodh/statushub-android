2a315062e0c87b110e2f88c92e4b45ae2bdc5c8d
Subodh Jena <jenasubodh@gmail.com>
2017-03-15 04:04:36 +0530

    Adds butterknife and socket io
    Adds butterknife and socket io

--- app/src/main/java/com/cuckoolabs/statushub/MainActivity.java
+++ app/src/main/java/com/cuckoolabs/statushub/MainActivity.java
@@ -3,11 +3,46 @@ package com.cuckoolabs.statushub;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.net.URISyntaxException;
+
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
+
 public class MainActivity extends AppCompatActivity {
+
+    Socket socket = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        try {
+            socket = IO.socket("https://statushub-dev.herokuapp.com/");
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
+
+            @Override
+            public void call(Object... args) {
+                socket.emit("foo", "hi");
+                socket.disconnect();
+            }
+
+        }).on("event", new Emitter.Listener() {
+
+            @Override
+            public void call(Object... args) {}
+
+        }).on(Socket.EVENT_DISCONNECT, new Emitter.Listener() {
+
+            @Override
+            public void call(Object... args) {}
+
+        });
+        socket.connect();
     }
 }
